bf6b9638bdcaf03769a862765b46ca14d1541701
DavidBrito32 <davidbrito.carneiro458@gmail.com>
2023-11-21 19:28:27 -0300

    envio inicial

---
+++ exercicios/process-argv.js
@@ -0,0 +1,4 @@
+console.log("Eu fui criado conforme solicitado!  ahh feito pelo nano");
+
+console.log("\n", process.argv[2], "\n", process.argv[3], "\n Todos os exercicios feiro conforme solicitado");
+
