da044e6ca41b60aa676597901cc2cbeb78a67299
DavidBrito32 <davidbrito.carneiro458@gmail.com>
2023-12-21 20:05:50 -0300

    Treino de sql

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,35 +1,121 @@
 -- Active: 1703025779984@@127.0.0.1@3306
 
--- CRIANDO A TABELA DE USUARIOS
-CREATE TABLE IF NOT EXISTS users(
-    id TEXT UNIQUE NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    created_at TEXT NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS users (
+        id TEXT UNIQUE NOT NULL PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        created_at TEXT NOT NULL
+    );
 
-INSERT INTO users(id, name, email, password, created_at)
+INSERT INTO
+    users (id, name, email, password, created_at)
 VALUES
-('u007', 'Maicon', 'maicon@email.com', '15487Ad*', CURRENT_TIMESTAMP);
+    (
+        'u007',
+        'Maicon',
+        'maicon@email.com',
+        '15487Ad*',
+        CURRENT_TIMESTAMP
+    );
 
-SELECT * FROM users;
+SELECT
+    *
+FROM
+    users;
+
+PRAGMA table_info (users);
 
 -- CRIANDO A TABELA DE PRODUTOS
+CREATE TABLE
+    IF NOT EXISTS products (
+        id TEXT NOT NULL UNIQUE PRIMARY KEY,
+        name TEXT NOT NULL,
+        price REAL NOT NULL,
+        description TEXT NOT NULL,
+        image_url TEXT NOT NULL
+    );
 
+SELECT
+    *
+FROM
+    products;
 
-CREATE TABLE IF NOT EXISTS products(
-    id TEXT NOT NULL UNIQUE PRIMARY KEY,
-    name TEXT NOT NULL,
-    price REAL NOT NULL,
-    description TEXT NOT NULL,
-    image_url TEXT NOT NULL
-);
-
-SELECT * FROM products;
-
-INSERT INTO products(id, name, price, description, image_url)
+INSERT INTO
+    products (id, name, price, description, image_url)
 VALUES
-('p001', 'Mouse Redragon', 332.45, 'Um mouse muito bom pelo que ele oferece', 'https://www.kabum.com.br/busca/mouse-redragon'),
-('p002', 'Intel Core I9 14900k', 5332.45, 'Melhor Processador da atualidade', 'https://www.kabum.com.br/busca/mouse-redragon'),
-('p003', 'Ryzen 7 7700', 2332.45, 'Processador Muito Robusto', 'https://www.kabum.com.br/busca/mouse-redragon');
+    (
+        'p008',
+        'Gamer hub engine',
+        25332.45,
+        'Melhor Engine Do momento',
+        'https://www.kabum.com.br/busca/gamer-hub'
+    );
+
+-- Exercicio 1
+SELECT
+    *
+FROM
+    users;
+
+-- GET ALL USERS
+-- Exercicio 2
+SELECT
+    *
+FROM
+    products;
+
+-- GET ALL PRODUCTS
+SELECT
+    *
+FROM
+    products
+WHERE
+    name LIKE '%gamer%';
+
+-- Exercicio 3
+INSERT INTO
+    users (id, name, email, password, created_at)
+VALUES
+    (
+        'u009',
+        'Leonardo',
+        'leonardo@email.com',
+        'leonardo1234',
+        CURRENT_TIMESTAMP
+    );
+
+    select * from users WHERE id = 'u003';
+
+-- INSERT NA TABELA USERS;
+INSERT INTO
+    products (id, name, price, description, image_url)
+VALUES
+    (
+        'p011',
+        'Produto',
+        20,
+        'Um produto',
+        'https://www.kabum.com.br/?product=produto'
+    );
+
+-- Create Product
+DELETE FROM users
+WHERE
+    id = 'u002'; -- Delete user
+
+DELETE FROM products
+WHERE
+    id = 'p002'; -- Delete product
+
+UPDATE products
+SET
+    name = 'Mudei de nome',
+    price = 455.58,
+    description = 'O melhor da categoria mudada',
+    image_url = 'https://www.kabum.com.br/busca/mouse-redragon'
+WHERE
+    id = 'p001'; -- Edit Product
+
+    select * from products;
